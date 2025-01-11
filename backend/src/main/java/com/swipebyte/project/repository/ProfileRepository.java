65df72d01124798a3b6322aabc7b57309e7c9ae8
Akash Mohan <akhikm4@gmail.com>
2025-01-10 18:38:25 -0600

    @nonnull annotation for profilerepo

--- backend/src/main/java/com/swipebyte/project/repository/ProfileRepository.java
+++ backend/src/main/java/com/swipebyte/project/repository/ProfileRepository.java
@@ -4,13 +4,15 @@ import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 
 import com.swipebyte.project.entity.*;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<UserProfile, Long> {
 
-    Optional<UserProfile> findById(Long id);
+    @NonNull
+    Optional<UserProfile> findById(@NonNull Long id);
 
     @Query("UPDATE UserProfile u SET u.bio = ?2 WHERE u.id = ?1")
     void updateBio(Long id, String newBio);
